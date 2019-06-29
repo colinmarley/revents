a7dc1919845f8bf890bd8772ac96e6e3a4058d65
colinmarley <colinmarley94@gmail.com>
2019-06-28 17:21:23 -0700

    section 6.4 complete

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -3,7 +3,7 @@ import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/navBar/NavBar';
 import { Container } from 'semantic-ui-react';
 import { Route } from 'react-router-dom';
-import homePage from '../../features/home/homePage';
+import HomePage from '../../features/home/HomePage';
 import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
 import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
 import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
@@ -15,17 +15,22 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <NavBar />
-          <Container className="main">
-            <Route exact path='/' component={ homePage } />
-            <Route path='/events' component={ EventDashboard } />
-            <Route path='/events/:id' component={ EventDetailedPage } />
-            <Route path='/people' component={ PeopleDashboard } />
-            <Route path='/profile/:id' component={ UserDetailedPage } />
-            <Route path='/settings' component={ SettingsDashboard } />
-            <Route path='/createEvent' component={ EventForm } />
-          </Container>
+        <Route exact path='/' component={ HomePage } />
+        <Route path='/(.+)' render={() => (
+          <Fragment>
+            <NavBar />
+              <Container className="main">
+                <Route path='/events' component={ EventDashboard } />
+                <Route path='/events/:id' component={ EventDetailedPage } />
+                <Route path='/people' component={ PeopleDashboard } />
+                <Route path='/profile/:id' component={ UserDetailedPage } />
+                <Route path='/settings' component={ SettingsDashboard } />
+                <Route path='/createEvent' component={ EventForm } />
+              </Container>
+          </Fragment>
+        )}/>
       </Fragment>
+      
     );
   }
 }
