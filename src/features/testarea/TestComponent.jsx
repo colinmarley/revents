85f82a08448d5f91481a4dee09360ed1560458c8
colinmarley <colinmarley94@gmail.com>
2019-07-01 13:27:13 -0700

    section 7.5 complete

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,20 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import { incrementCounter, decrementCounter } from './testActions';
+import { Button } from 'semantic-ui-react';
 
 const mapState = (state, ownProps) => ({
     data: state.data
 });
 
 const mapDispatch = dispatch => ({
-
+    incrementCounter: () => { dispatch(incrementCounter()); },
+    decrementCounter: () => { dispatch(decrementCounter()); }
 });
 
 class TestComponent extends Component {
+
     render() {
+        const { data, incrementCounter, decrementCounter } = this.props;
         return (
             <div>
                 <h1>Test Component</h1>
-                <h3>The Answer is { this.props.data } </h3>
+                <h3>The Answer is { data } </h3>
+                <Button onClick={incrementCounter} positive content='Increment' />
+                <Button onClick={decrementCounter} negative content='Decrement' />
             </div>
         )
     }
