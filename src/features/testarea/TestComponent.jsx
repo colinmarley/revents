b159fa950f56290b11a650dea4019f3a3fbe37ef
colinmarley <colinmarley94@gmail.com>
2019-07-01 13:13:27 -0700

    section 7.4 complete

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+const mapState = (state, ownProps) => ({
+    data: state.data
+});
+
+const mapDispatch = dispatch => ({
+
+});
 
 class TestComponent extends Component {
     render() {
         return (
             <div>
                 <h1>Test Component</h1>
+                <h3>The Answer is { this.props.data } </h3>
             </div>
         )
     }
 }
 
-export default TestComponent;
+export default connect(
+    mapState,
+    mapDispatch
+)(TestComponent);
