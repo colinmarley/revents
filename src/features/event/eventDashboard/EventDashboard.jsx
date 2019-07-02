978a4794981a1a73d03463557f2c5b10cd2dafbf
colinmarley <colinmarley94@gmail.com>
2019-07-01 17:24:05 -0700

    section 8 complete

--- src/features/event/eventDashboard/EventDashboard.jsx
+++ src/features/event/eventDashboard/EventDashboard.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Grid } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import EventList from '../EventList/EventList';
-import cuid from 'cuid';
 import { createEvent, deleteEvent, updateEvent } from '../eventActions';
 
 const mapState = (state, ownProps) => ({
@@ -17,17 +16,6 @@ const mapDispatch = dispatch => ({
 
 
 class EventDashboard extends Component {
-
-    handleCreateEvent = newEvent => {
-        newEvent.id = cuid();
-        newEvent.hostPhotoURL = '/assets/user.png';
-
-        this.props.createEvent(newEvent);
-    }
-
-    handleUpdateEvent = updatedEvent => {
-        this.props.updateEvent(updatedEvent);
-    }
 
     handleDeleteEvent = id => {
         this.props.deleteEvent(id);
