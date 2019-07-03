db9976610a5e2b770c7678440fa2da384c274aec
colinmarley <colinmarley94@gmail.com>
2019-07-03 15:42:06 -0700

    section 10.6 complete

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { incrementCounter, decrementCounter } from './testActions';
+import { geocodeByAddress, getLatLng} from 'react-places-autocomplete';
 import { Button } from 'semantic-ui-react';
 import TestPlaceInput from './TestPlaceInput';
+import SimpleMap from './SimpleMap';
 
 const mapState = (state, ownProps) => ({
 	data: state.test.data,
@@ -18,7 +20,26 @@ const mapDispatch = dispatch => ({
 });
 
 class TestComponent extends Component {
+
+	state = {
+		latlng: {
+			lat: 69.00,
+			lng: 69.00
+		}
+	}
+
+	handleSelect = address => {
+		geocodeByAddress(address)
+			.then(results => getLatLng(results[0]))
+			.then(latlng =>
+				this.setState({
+					latlng: latlng,
+				})
+			);
+	};
+
 	render() {
+		const { latlng } = this.state;
 		const { data, incrementCounter, decrementCounter } = this.props;
 		return (
 			<div>
@@ -26,7 +47,8 @@ class TestComponent extends Component {
 				<h3>The Answer is {data} </h3>
 				<Button onClick={incrementCounter} positive content='Increment' />
 				<Button onClick={decrementCounter} negative content='Decrement' />
-				<TestPlaceInput />
+				<TestPlaceInput handleSelect={this.handleSelect} />
+				<SimpleMap latlng={latlng} />
 			</div>
 		);
 	}
--- src/features/testarea/TestPlaceInput.jsx
+++ src/features/testarea/TestPlaceInput.jsx
@@ -7,26 +7,23 @@ import PlacesAutocomplete, {
 class TestPlaceInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { address: '' };
+    this.state = {
+      address: '',
+      latlng: {},
+    };
   }
 
   handleChange = address => {
     this.setState({ address });
   };
 
-  handleSelect = address => {
-    geocodeByAddress(address)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => console.log('Success', latLng))
-      .catch(error => console.error('Error', error));
-  };
-
   render() {
+    const {handleSelect} = this.props;
     return (
       <PlacesAutocomplete
         value={this.state.address}
         onChange={this.handleChange}
-        onSelect={this.handleSelect}
+        onSelect={handleSelect}
       >
         {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
           <div>
