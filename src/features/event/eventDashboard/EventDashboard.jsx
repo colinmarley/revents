70aa42550c6b80f6d7270d947dbbce74b705f2c1
colinmarley <colinmarley94@gmail.com>
2019-07-09 17:16:30 -0700

    section 12 complete

--- src/features/event/eventDashboard/EventDashboard.jsx
+++ src/features/event/eventDashboard/EventDashboard.jsx
@@ -3,9 +3,11 @@ import { Grid } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import EventList from '../EventList/EventList';
 import { createEvent, deleteEvent, updateEvent } from '../eventActions';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 const mapState = (state, ownProps) => ({
 	events: state.events,
+	loading: state.async.loading,
 });
 
 const mapDispatch = dispatch => ({
@@ -26,7 +28,8 @@ class EventDashboard extends Component {
 	};
 
 	render() {
-		const { events } = this.props;
+		const { events, loading } = this.props;
+		if (loading) return <LoadingComponent />
 		return (
 			<Grid>
 				<Grid.Column width={10}>
