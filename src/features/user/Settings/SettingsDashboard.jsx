cf70da412aa0e8cc22814dd6a0d3ef922919264d
colinmarley <colinmarley94@gmail.com>
2019-07-12 15:31:14 -0700

    section 15-14 complete

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Grid } from 'semantic-ui-react';
 import SettingsNav from './SettingsNav';
 import { Route, Redirect, Switch } from 'react-router-dom';
@@ -6,8 +7,13 @@ import BasicPage from './BasicPage';
 import AboutPage from './AboutPage';
 import PhotosPage from './PhotosPage';
 import AccountPage from './AccountPage';
+import { updatePassword } from '../../auth/authActions';
 
-const SettingsDashboard = () => {
+const mapDispatch = {
+	updatePassword
+}
+
+const SettingsDashboard = ({ updatePassword }) => {
 	return (
 		<Grid>
 			<Grid.Column width={12}>
@@ -16,7 +22,7 @@ const SettingsDashboard = () => {
 					<Route path='/settings/basic' component={BasicPage} />
 					<Route path='/settings/about' component={AboutPage} />
 					<Route path='/settings/photos' component={PhotosPage} />
-					<Route path='/settings/account' component={AccountPage} />
+					<Route path='/settings/account' render={() => <AccountPage updatePassword={updatePassword} />}/>
 				</Switch>
 			</Grid.Column>
 			<Grid.Column width={4}>
@@ -26,4 +32,4 @@ const SettingsDashboard = () => {
 	);
 };
 
-export default SettingsDashboard;
+export default connect(null, mapDispatch)(SettingsDashboard);
