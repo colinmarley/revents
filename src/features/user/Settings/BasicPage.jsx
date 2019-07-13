b8dd7dc50a0f3be0a8e93d5fead828dd21ffe182
colinmarley <colinmarley94@gmail.com>
2019-07-12 17:43:04 -0700

    section 16.3 complete

--- src/features/user/Settings/BasicPage.jsx
+++ src/features/user/Settings/BasicPage.jsx
@@ -1,14 +1,22 @@
-import React, {Component} from 'react';
-import {Segment, Form, Header, Divider, Button} from 'semantic-ui-react';
-import {Field, reduxForm} from 'redux-form';
-import DateInput from "../../../app/common/form/DateInput";
-import PlaceInput from "../../../app/common/form/PlaceInput";
-import TextInput from "../../../app/common/form/TextInput";
+import React, { Component } from 'react';
+import {
+	Segment,
+	Form,
+	Header,
+	Divider,
+	Button,
+	Label,
+} from 'semantic-ui-react';
+import { addYears } from 'date-fns';
+import { Field, reduxForm } from 'redux-form';
+import DateInput from '../../../app/common/form/DateInput';
+import PlaceInput from '../../../app/common/form/PlaceInput';
+import TextInput from '../../../app/common/form/TextInput';
+import RadioInput from '../../../app/common/form/RadioInput';
 
 class BasicPage extends Component {
-
 	render() {
-		const {pristine, submitting} = this.props;
+		const { pristine, submitting } = this.props;
 		return (
 			<Segment>
 				<Header dividing size='large' content='Basics' />
@@ -21,28 +29,54 @@ class BasicPage extends Component {
 						placeholder='Known As'
 					/>
 					<Form.Group inline>
-					  {/* todo: Gender Radio button */}
+						<label >Gender: </label>
+						<Field
+							name='gender'
+							type='radio'
+							value='male'
+							label='Male'
+							component={RadioInput}
+						/>
+						<Field
+							name='gender'
+							type='radio'
+							value='femal'
+							label='Female'
+							component={RadioInput}
+						/>
 					</Form.Group>
 					<Field
 						width={8}
 						name='dateOfBirth'
 						component={DateInput}
 						placeholder='Date of Birth'
+						dateFormat='dd LLL yyyy'
+						showYearDropdown={true}
+						showMonthDropdown={true}
+						dropdownMode='select'
+						maxDate={addYears(new Date(), -18)}
 					/>
 					<Field
 						name='city'
 						placeholder='Home Town'
-						options={{types: ['(cities)']}}
+						options={{ types: ['(cities)'] }}
 						label='Female'
 						component={PlaceInput}
 						width={8}
 					/>
-					<Divider/>
-					<Button disabled={pristine || submitting} size='large' positive content='Update Profile'/>
+					<Divider />
+					<Button
+						disabled={pristine || submitting}
+						size='large'
+						positive
+						content='Update Profile'
+					/>
 				</Form>
 			</Segment>
 		);
 	}
 }
 
-export default reduxForm({form: 'userProfile', enableReinitialize: true})(BasicPage);
+export default reduxForm({ form: 'userProfile', enableReinitialize: true })(
+	BasicPage
+);
