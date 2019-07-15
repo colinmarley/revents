23d2e53b2aa089b063bf03ce4950c759caf99128
colinmarley <colinmarley94@gmail.com>
2019-07-15 13:21:54 -0700

    section 16.6 complete

--- src/app/common/form/DateInput.jsx
+++ src/app/common/form/DateInput.jsx
@@ -4,22 +4,27 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const DateInput = ({
-	input,
+	input: { value, onChange, onBlur },
 	width,
 	placeholder,
 	meta: { touched, error },
 	...rest
 }) => {
-	
 	return (
 		<Form.Field error={touched && !!error}>
 			<DatePicker
 				{...rest}
 				placeholderText={placeholder}
-				selected={input.value? new Date(input.value) : null}
-                onChange={input.onChange}
-                onBlur={input.onBlur}
-                onChangeRaw={(e) => e.preventDefault()}
+				selected={
+					value
+						? Object.prototype.toString.call(value) !== '[object Date]'
+							? value.toDate()
+							: value
+						: null
+				}
+				onChange={onChange}
+				onBlur={(e, val) => onBlur(val)}
+				onChangeRaw={e => e.preventDefault()}
 			/>
 			{touched && error && (
 				<Label basic color='red'>
