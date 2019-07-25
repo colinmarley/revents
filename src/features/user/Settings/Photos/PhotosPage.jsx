cb13751953248d98a22c81a0f537f0074304f5ff
colinmarley <colinmarley94@gmail.com>
2019-07-25 13:04:29 -0700

    section 17 complete

--- src/features/user/Settings/Photos/PhotosPage.jsx
+++ src/features/user/Settings/Photos/PhotosPage.jsx
@@ -29,7 +29,8 @@ const query = ({auth}) => {
 const mapState = (state) => ({
 	auth: state.firebase.auth,
 	profile: state.firebase.profile,
-	photos: state.firestore.ordered.photos
+	photos: state.firestore.ordered.photos,
+	loading: state.async.loading,
 });
 
 const mapDispatch = {
@@ -38,7 +39,7 @@ const mapDispatch = {
 	setMainPhoto
 };
 
-const PhotosPage = ({ uploadProfileImage, photos, profile, deletePhoto, setMainPhoto }) => {
+const PhotosPage = ({ uploadProfileImage, photos, profile, deletePhoto, setMainPhoto, loading }) => {
 	const [files, setFiles] = useState([]);
 	const [image, setImage] = useState([]);
 
@@ -111,12 +112,14 @@ const PhotosPage = ({ uploadProfileImage, photos, profile, deletePhoto, setMainP
 							/>
 							<Button.Group>
 								<Button
+									loading={loading}
 									onClick={handleUploadImage}
 									style={{ width: '100px' }}
 									positive
 									icon='check'
 								/>
 								<Button
+									disabled={loading}
 									onClick={handleCancelCrop}
 									style={{ width: '100px' }}
 									icon='close'
